8a6256842c0130f3ce65063250fb14de718a529e
Leberwurscht <leberwurscht@hoegners.de>
2010-11-15 13:28:25 +0100

    made testtrie.ml clearer

--- testtrie.ml
+++ testtrie.ml
@@ -1,6 +1,10 @@
 open PTreeDB;;
 
+(***** prefix tree initialisation *****)
+
 module ZSet = ZZp.Set;;
+
+let timeout = !Settings.reconciliation_config_timeout;;
 
 let settings = { (* copied from reconserver.ml *)
 	mbar = !Settings.mbar;
@@ -18,12 +22,7 @@ let settings = { (* copied from reconserver.ml *)
 init_db settings;;
 init_ptree settings;;
 
-let add_number number =
-	Printf.printf "got %s\n" (Number.to_string (ZZp.to_number number));
-	let txn = new_txnopt () in
-		PTree.insert (get_ptree ()) txn number;
-		PTree.clean txn (get_ptree ());
-		commit_txnopt txn
+(***** initialize unix domain sockets *****)
 
 (* create recon server socket *)
 let unixsockaddr = "server.ocaml2py.sock";;
@@ -46,8 +45,33 @@ let addsock = Eventloop.create_sock addr;;
 (* socket to transmit the hashes received from other servers to python *)
 let othersockaddr = "hashes.ocaml2py.sock";;
 
-let timeout = !Settings.reconciliation_config_timeout;;
+(***** helper functions *****)
 
+(* function to iterate over lines of an input stream *)
+let rec readline_rec cin buffer = 
+	let c = cin#read_char in (* read character *)
+	if c='\n' then (
+		buffer (* if it's a newline, return the buffer obtained by now *)
+	) else (
+		Buffer.add_char buffer c; (* otherwise, add the character to the buffer *)
+		readline_rec cin buffer (* and continue reading *)
+	);;	
+
+let readline cin = 
+	let buffer = Buffer.create 32 in (* create empty buffer with size estimation *)
+	let buffer = readline_rec cin buffer in (* fill buffer with received characters *)
+	let str	= Buffer.sub buffer 0 (Buffer.length buffer) in (* convert buffer to string *)
+	str;; (* return string *)
+
+let rec iter_lines cin callback =
+	try (
+		let line = readline cin in (* read a line *)
+			callback line; (* call the callback *)
+			iter_lines cin callback (* and continue reading lines *)
+	) with End_of_file -> ();; (* except end of file is reached *)
+
+(* function to convert the numbers received from other servers to hashes and send
+   them to hashes.ocaml2py.sock *)
 let send_number cout number =
 	let hash = RMisc.truncate (ZZp.to_bytes number) KeyHash.hash_bytes in
 	let hexhash = KeyHash.hexify hash in
@@ -68,47 +92,7 @@ let send_numbers numbers =
 	Unix.close socket;
 	Common.plerror 1 "Connection to hashes.ocaml2py.sock closed";;
 
-let testserver addr cin cout =
-	Common.plerror 1 "connection on server.ocaml2py.sock";
-	let cin = (new Channel.sys_in_channel cin)
-	and cout = (new Channel.sys_out_channel cout) in
-		let data = Client.handle (get_ptree ()) cin cout in
-		send_numbers data;
-
-	Common.plerror 1 "did synchronisation as server as requested by %s" (ReconMessages.sockaddr_to_string addr);
-	[];;
-
-let testclient addr cin cout =
-	Common.plerror 1 "connection on client.ocaml2py.sock";
-	let cin = (new Channel.sys_in_channel cin)
-	and cout = (new Channel.sys_out_channel cout) in
-		let data = Server.handle (get_ptree ()) cin cout in
-		send_numbers data;
-
-	Common.plerror 1 "did synchronisation as client as requested by %s" (ReconMessages.sockaddr_to_string addr);
-	[];;
-
-let rec readline_rec cin buffer = 
-	let c = cin#read_char in (* read character *)
-	if c='\n' then (
-		buffer (* if it's a newline, return the buffer obtained by now *)
-	) else (
-		Buffer.add_char buffer c; (* otherwise, add the character to the buffer *)
-		readline_rec cin buffer (* and continue reading *)
-	);;	
-
-let readline cin = 
-	let buffer = Buffer.create 32 in (* create empty buffer with size estimation *)
-	let buffer = readline_rec cin buffer in (* fill buffer with received characters *)
-	let str	= Buffer.sub buffer 0 (Buffer.length buffer) in (* convert buffer to string *)
-	str (* return string *)
-
-let rec iter_lines cin callback =
-	try (
-		let line = readline cin in (* read a line *)
-			callback line; (* call the callback *)
-			iter_lines cin callback (* and continue reading lines *)
-	) with End_of_file -> ();; (* except end of file is reached *)
+(* function to add a hash to the database *)
 
 let add_hash hexhash =
 	let l = String.length hexhash in
@@ -125,8 +109,30 @@ let add_hash hexhash =
 		Common.plerror 1 "received line with invalid length %d" l;
 	)
 
-let testadd addr cin cout =
-	Common.plerror 1 "connection on add.ocaml2py.sock";
+(***** eventloop callbacks *****)
+
+let servercb addr cin cout =
+	Common.plerror 1 "connection on server.ocaml2py.sock";
+	let cin = (new Channel.sys_in_channel cin)
+	and cout = (new Channel.sys_out_channel cout) in
+		let data = Client.handle (get_ptree ()) cin cout in
+		send_numbers data;
+
+	Common.plerror 1 "did synchronisation as server as requested by %s" (ReconMessages.sockaddr_to_string addr);
+	[];;
+
+let clientcb addr cin cout =
+	Common.plerror 1 "connection on client.ocaml2py.sock";
+	let cin = (new Channel.sys_in_channel cin)
+	and cout = (new Channel.sys_out_channel cout) in
+		let data = Server.handle (get_ptree ()) cin cout in
+		send_numbers data;
+
+	Common.plerror 1 "did synchronisation as client as requested by %s" (ReconMessages.sockaddr_to_string addr);
+	[];;
+
+let addcb addr cin cout =
+	Common.plerror 1 "got connection on add.ocaml2py.sock";
 	let cin = (new Channel.sys_in_channel cin)
 	and cout = (new Channel.sys_out_channel cout) in
 		iter_lines cin add_hash;
@@ -134,8 +140,10 @@ let testadd addr cin cout =
 
 	[];;
 
+(***** start the event loop *****)
+
 Eventloop.evloop [] [
-	(serversock, Eventloop.make_th ~name:"testserver" ~cb:testserver ~timeout:timeout);
-	(clientsock, Eventloop.make_th ~name:"testclient" ~cb:testclient ~timeout:timeout);
-	(addsock, Eventloop.make_th ~name:"testadd" ~cb:testadd ~timeout:timeout)
+	(serversock, Eventloop.make_th ~name:"serverhandler" ~cb:servercb ~timeout:timeout);
+	(clientsock, Eventloop.make_th ~name:"clienthandler" ~cb:clientcb ~timeout:timeout);
+	(addsock, Eventloop.make_th ~name:"addhandler" ~cb:addcb ~timeout:timeout)
 ];;
