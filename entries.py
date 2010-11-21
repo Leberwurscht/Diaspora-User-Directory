c2c6560ec9206561ca0e0e550a1d4a814a178127
Leberwurscht <leberwurscht@hoegners.de>
2010-11-22 00:00:04 +0100

    added a EntryServer allowing to acquire EntryLists over the network

--- entries.py
+++ entries.py
@@ -7,10 +7,12 @@ import socket
 import time
 import binascii, hashlib
 
+import json
+
 import sqlite3 as db
 
 def get_db_connection():
-    return db.connect("entries.sqlite")
+    return db.connect("entries.sqlite", check_same_thread=False)
 
 # check if table exists and create it if not
 
@@ -55,24 +57,146 @@ def signature_valid(public_key, signature, text):
 
 ######
 
+import threading
+
+class EntryServer(threading.Thread):
+    def __init__(self, con, interface="localhost", port=20001):
+            threading.Thread.__init__(self)
+
+            self.con = con
+
+            entrysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            entrysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            entrysocket.bind((interface,port))
+            entrysocket.listen(5)
+
+            self.entrysocket = entrysocket
+
+            self.daemon = True  # terminate if main program exits
+            self.start()
+
+    def run(self):
+        while True:
+            (clientsocket, address) = self.entrysocket.accept()
+
+            thread = threading.Thread(
+                target=self.handle_connection,
+                args=(clientsocket, address)
+            )
+
+            thread.start()
+
+    def handle_connection(self, clientsocket, address):
+        cur = self.con.cursor()
+        f = clientsocket.makefile()
+
+        # build list of entries to be transmitted
+        entrylist = EntryList()
+
+        while True:
+            hexhash = f.readline().strip()
+            if not hexhash: break
+
+            binhash = binascii.unhexlify(hexhash)
+            entry = Entry.from_database(cur, binhash)
+
+            if entry: entrylist.append(entry)
+
+        # serve requested hashes
+        json_string = entrylist.json()
+        f.write(json_string)
+        f.close()
+        clientsocket.close()
+
+######
+
+class InvalidHashError(Exception): pass
+
+class EntryList(list):
+    @classmethod
+    def from_server(cls, binhashes, host, port=20001):
+        asocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        asocket.connect((host, port))
+
+        for binhash in binhashes:
+            hexhash = binascii.hexlify(binhash)
+            asocket.sendall(hexhash+"\n")
+        asocket.sendall("\n")
+
+        f = asocket.makefile()
+        json_string = f.read()
+        f.close()
+        asocket.close()
+
+        return cls.from_json(json_string)
+
+    @classmethod
+    def from_json(cls, json_string):
+        entrylist = cls()
+
+        for json_entry in json.loads(json_string):
+            webfinger_address = json_entry["webfinger_address"]
+            full_name = json_entry["full_name"]
+            hometown = json_entry["hometown"]
+            country_code = json_entry["country_code"]
+            captcha_signature = binascii.unhexlify(json_entry["captcha_signature"])
+
+            if "timestamp" in json_entry:
+                timestamp = json_entry["timestamp"]
+            else:
+                timestamp = None
+
+            entry = Entry(webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp)
+
+            if "hash" in json_entry:
+                if not binascii.unhexlify(json_entry["hash"])==entry.hash:
+                    raise InvalidHashError
+
+            entrylist.append(entry)
+
+        return entrylist
+
+    def json(self):
+        json_list = []
+
+        for entry in self:
+            json_list.append({
+                "hash":binascii.hexlify(entry.hash),
+                "webfinger_address":entry.webfinger_address,
+                "full_name":entry.full_name,
+                "hometown":entry.hometown,
+                "country_code":entry.country_code,
+                "captcha_signature":binascii.hexlify(entry.captcha_signature),
+                "timestamp":entry.timestamp
+            })
+
+        json_string = json.dumps(json_list)
+
+        return json_string
+
 class Entry:
     """ represents a database entry for a webfinger address """
 
     @classmethod
-    def from_hash(cls, entryhash):
-        raise NotImplementedError, "Not implemented yet."
+    def from_database(cls, cur, binhash):
+        cur.execute(
+            "SELECT webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp FROM entries WHERE hash=?",
+            (buffer(binhash), )
+        )
+        args = cur.fetchone()
+        if not args: return None
+
+        entry = cls(*args)
+
+        return entry
 
     @classmethod
-    def from_webfinger_address(cls, webfinger_address):
-        raise NotImplementedError, "Not implemented yet."
-
-    @classmethod
-    def fetch(cls, webfinger_address):
+    def from_webfinger_profile(cls, webfinger_address):
         # https://github.com/jcarbaugh/python-webfinger
         raise NotImplementedError, "Not implemented yet."
 
     def __init__(self, webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp=None):
-        """ arguments must be utf-8 encoded strings (except timestamp) """
+        """ arguments must be unicode strings, except captcha_signature (buffer) and timestamp (int) """
         if not timestamp: timestamp = int(time.time())
 
         self.webfinger_address = webfinger_address
@@ -93,7 +217,7 @@ class Entry:
         self.hash = combinedhash.digest()[:16]
 
     def captcha_signature_valid(self):
-        return signature_valid(captcha_key, self.captcha_signature, self.webfinger_address)
+        return signature_valid(captcha_key, self.captcha_signature, self.webfinger_address.encode("utf-8"))
 
     def save(self, cur, con=None):
         cur.execute("INSERT INTO entries (hash, webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp) VALUES (?,?,?,?,?,?,?)",
--- test.py
+++ test.py
@@ -19,18 +19,53 @@ def sign(private_key, text):
 
 private_key = get_private_key()
 
-import entries, binascii
+import entries, binascii, sqlite3
 
-webfinger_address = "test@example.com"
-full_name = "John Doe"
-hometown = "Los Angeles"
-country_code = "US"
-captcha_signature = sign(private_key, webfinger_address)
+# create an example database entry
+webfinger_address = u"test@example.com"
+full_name = u"John Doe"
+hometown = u"Los Angeles"
+country_code = u"US"
+captcha_signature = sign(private_key, webfinger_address.encode("utf-8"))
 timestamp = 1290117971
 
-e = entries.Entry(webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp)
-assert e.captcha_signature_valid()
-con = entries.get_db_connection()
-e.save(con.cursor(), con)
+entry = entries.Entry(webfinger_address, full_name, hometown, country_code, captcha_signature, timestamp)
+assert entry.captcha_signature_valid()
 
-print binascii.hexlify(e.hash)
+# save it
+con = entries.get_db_connection()
+
+try:
+    entry.save(con.cursor(), con)
+except sqlite3.IntegrityError:
+    print "already in db"
+
+# print hash
+binhash = entry.hash
+hexhash = binascii.hexlify(binhash)
+print hexhash
+
+# test loading from database
+entry2 = entries.Entry.from_database(con.cursor(), binhash)
+hexhash2 = binascii.hexlify(entry2.hash)
+assert hexhash2==hexhash
+
+# test exporting a EntryList as JSON
+entrylist = entries.EntryList([entry])
+json_string = entrylist.json()
+print json_string
+
+# test loading a EntryList from JSON
+entrylist2 = entries.EntryList.from_json(json_string)
+entry3 = entrylist2[0]
+hexhash3 = binascii.hexlify(entry3.hash)
+assert hexhash3==hexhash
+
+# run a EntryServer
+entryserver = entries.EntryServer(con)
+
+# get a EntryList from the EntryServer
+entrylist3 = entries.EntryList.from_server([binhash], "localhost")
+entry4 = entrylist3[0]
+hexhash4 = binascii.hexlify(entry4.hash)
+assert hexhash4==hexhash
