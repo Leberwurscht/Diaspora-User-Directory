9d4d6c6312702448460f39e41bbfa14559168f27
Leberwurscht <leberwurscht@hoegners.de>
2011-03-11 00:19:13 +0100

    added TwoPriorityQueue

--- lib.py
+++ lib.py
@@ -52,3 +52,77 @@ class BaseServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     def terminate(self):
         self.shutdown()
         self.socket.close()
+
+import Queue, threading
+from Queue import PriorityQueue, Full
+
+class TwoPriorityQueue:
+    """ For Queue-like objects accepting two types of items: high-priority and low-priority ones.
+        For both types, separate maxsizes can be specified. """
+
+    def __init__(self, maxsize_low=0, maxsize_high=0):
+        self.queue = PriorityQueue()
+
+        self.size_lock = threading.Lock()
+
+        self.size_low = 0
+        self.maxsize_low = maxsize_low
+
+        self.size_high = 0
+        self.maxsize_high = maxsize_high
+
+    def put_item(self, item):
+        priority = item[0]
+
+        self.size_lock.acquire()
+
+        try:
+            if priority>0:
+                # low-priority
+                if self.maxsize_low>0 and self.size_low >= self.maxsize_low:
+                    raise Full
+
+                self.queue.put(item)
+                self.size_low += 1
+            else:
+                # high-priority
+                if self.maxsize_high>0 and self.size_high >= self.maxsize_high:
+                    raise Full
+
+                self.queue.put(item)
+                self.size_high += 1
+        finally:
+            self.size_lock.release()
+
+    def get_item(self):
+        item = self.queue.get()
+
+        priority = item[0]
+
+        self.size_lock.acquire()
+
+        if priority>0:
+            # low-priority
+            self.size_low -= 1
+        else:
+            # high-priority
+            self.size_high -= 1
+
+        self.size_lock.release()
+
+        return item
+
+    def put_high(self, item):
+        self.put_item((0, item))
+
+    def put_low(self, item):
+        self.put_item((1, item))
+
+    def get(self):
+        return self.get_item()[1]
+
+    def task_done(self):
+        self.queue.task_done()
+
+    def join(self):
+        self.queue.join()
