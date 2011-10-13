aac30daad2d54bc44c357db467395ba8ead42274
Leberwurscht <leberwurscht@hoegners.de>
2011-10-14 01:35:26 +0200

    add signature checking to lib.py

--- lib.py
+++ lib.py
@@ -168,6 +168,22 @@ class BaseServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
         self.socket.close()
 
 ###
+# signature checking
+
+import base64, paramiko
+
+def signature_valid(public_key_base64, signature, text):
+    data = base64.decodestring(public_key_base64)
+    public_key = paramiko.RSAKey(data=data)
+
+    sig_message = paramiko.Message()
+    sig_message.add_string("ssh-rsa")
+    sig_message.add_string(signature)
+    sig_message.rewind()
+
+    return public_key.verify_ssh_sig(text, sig_message)
+
+###
 # Implementation of a cron-like scheduler
 
 """
