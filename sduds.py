e75d54a944a6477bf3542047e4361844fd1c853e
Leberwurscht <leberwurscht@hoegners.de>
2012-02-28 16:42:24 +0100

    Command line interface of sduds.py

--- sduds.py
+++ sduds.py
@@ -389,12 +389,14 @@ if __name__=="__main__":
     import optparse, sys
 
     parser = optparse.OptionParser(
-        usage = "%prog  [-p WEBSERVER_PORT] [-s SYNCHRONIZATION_PORT] [PARTNER]",
+        usage = "%prog  [-p WEBSERVER_PORT] [-s SYNCHRONIZATION_PORT] [-f FQDN] [PARTNER]",
         description="run a sduds server or connect manually to another one"
     )
     
     parser.add_option( "-p", "--webserver-port", metavar="PORT", dest="webserver_port", help="the webserver port of the own server")
     parser.add_option( "-s", "--synchronization-port", metavar="PORT", dest="synchronization_port", help="the synchronization port of the own server")
+    parser.add_option( "-f", "--fqdn", metavar="FQDN", dest="fqdn", default="localhost", help="the fully qualified domain name of the system")
+    # TODO: If fqdn==None, guess automatically in WebServer
 
     (options, args) = parser.parse_args()
 
@@ -416,6 +418,12 @@ if __name__=="__main__":
 
     interface = "localhost"
 
+    context = Context(partnerdb_path="partners.sqlite", statedb_path="states.sqlite", hashtrie_path="PTree")
+    sduds = Application(context)
+
+    sduds.configure_workers()
+    sduds.configure_web_server(interface, webserver_port)
+
     if len(args)>0:
         ### initiate connection if a partner is passed
         try:
@@ -424,26 +432,19 @@ if __name__=="__main__":
             print >>sys.stderr, "Invalid number of arguments."
             sys.exit(1)
 
-        # create SDUDS instance
-        sduds = SDUDS(("localhost", webserver_port))
+        sduds.configure_jobs(synchronization=False, partnerdb_cleanup=False, statedb_cleanup=True)
+        sduds.start(synchronization_server=False)
 
         # synchronize with another server
-        server = partners.Server.from_database(sduds.context.partnerdb, partner_name=partner_name)
-
-        if not server:
+        if not context.partnerdb.get_partner(partner_name):
             print >>sys.stderr, "Unknown server - add it with partners.py."
+            sduds.terminate()
             sys.exit(1)
-
-        if server.kicked():
-            print >>sys.stderr, "Will not connect - server is kicked!"
-            sys.exit(1)
-
-        sduds.run_synchronization_server("localhost", interface, synchronization_port)
 
         try:
-            sduds.synchronize_with_partner(server)
+            sduds.synchronize_with_partner(partner_name)
         except socket.error,e:
-            print >>sys.stderr, "Connecting to %s failed: %s" % (str(server), str(e))
+            print >>sys.stderr, "Connecting to %s failed: %s" % (partner_name, str(e))
             sys.exit(1)
 
         sduds.terminate()
@@ -451,8 +452,9 @@ if __name__=="__main__":
     else:
         ### otherwise simply run a server
 
-        # start servers
-        sduds = SDUDS((interface, webserver_port))
+        sduds.configure_jobs()
+        sduds.configure_synchronization_server(options.fqdn, "", synchronization_port)
+        sduds.start()
 
         # define exitfunc
         def exit():
@@ -472,9 +474,6 @@ if __name__=="__main__":
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
         signal.signal(signal.SIGHUP, signal_handler)
-
-        # run the synchronization server
-        sduds.run_synchronization_server("localhost", interface, synchronization_port)
 
         # wait until program is interrupted
         while True: time.sleep(100)
